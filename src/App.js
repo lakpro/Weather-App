fa161328e97a42c749808fd6311ec13dc34a6d3c
Lakshay Setia <lakshaysetia123@gmail.com>
2023-03-12 22:01:51 +0530

    added skeleton loading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import { useDebugValue, useEffect, useState } from "react";
 import axios from "axios";
-import moment, { duration } from "moment";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 function App() {
   const [city, setCity] = useState("");
@@ -15,7 +16,7 @@ function App() {
   const [hour, setHour] = useState(new Date().getHours());
   const [greeting, setGreeting] = useState();
   const [bg, setBg] = useState("/pics/welcome.jpg");
-  const [display, setDisplay] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => setFirst(true), first);
 
@@ -28,6 +29,7 @@ function App() {
   async function handleSubmit(event) {
     event.preventDefault();
     // axios.get().then((data) => {});
+    setIsLoading(true);
     const { data } = await axios.post(
       "https://weather-app-server-yolm.onrender.com/",
       {
@@ -56,7 +58,7 @@ function App() {
     setBg(() => {
       return data.error ? `/pics/welcome.jpg` : `/pics/${data.icon}.jpg`;
     });
-
+    setIsLoading(false);
     console.log(bg);
   }
 
@@ -66,37 +68,76 @@ function App() {
 
   return (
     <div className="main" style={{ backgroundImage: `url(${bg})` }}>
-      <div className="box">
-        <h1>Hey, Good {greeting}</h1>
-        {(error || first) && (
-          <p className="greeting">WELCOME TO THE WEATHER APP!!</p>
-        )}
-        <form className="form">
-          <input
-            className="input"
-            type="text"
-            name="city"
-            placeholder={placeholder}
-            onChange={handleChange}
-            value={city}
-          />
-          <button className="btn" onClick={handleSubmit}>
-            SUBMIT
-          </button>
-        </form>
-        <div className="data">
-          {!error && location && <h1>Location: {location} </h1>}
-          {!error && weather && <h2>Weather: {weather}</h2>}
-          {!error && temp && <h3>Temp: {temp}°C</h3>}
-          {!error && url && <img className="img" src={url} />}
-        </div>
-        <div className="data2">
-          {!first && error && (
-            <h1 className="text">Please enter correct City</h1>
+      {!isLoading ? (
+        <div className="box">
+          <h1>Hey, Good {greeting}</h1>
+          {(error || first) && (
+            <p className="greeting">WELCOME TO THE WEATHER APP!!</p>
           )}
-          {first && <h1 className="text">Please enter a City</h1>}
+          <form className="form">
+            <input
+              className="input"
+              type="text"
+              name="city"
+              placeholder={placeholder}
+              onChange={handleChange}
+              value={city}
+            />
+            <button className="btn" onClick={handleSubmit}>
+              SUBMIT
+            </button>
+          </form>
+          <div className="data">
+            {!error && location && <h1>Location: {location} </h1>}
+            {!error && weather && <h2>Weather: {weather}</h2>}
+            {!error && temp && <h3>Temp: {temp}°C</h3>}
+            {!error && url && <img className="img" src={url} />}
+          </div>
+          <div className="data2">
+            {!first && error && (
+              <h1 className="text">Please enter correct City</h1>
+            )}
+            {first && <h1 className="text">Please enter a City</h1>}
+          </div>
         </div>
-      </div>
+      ) : (
+        <SkeletonTheme baseColor="#202020" highlightColor="#444">
+          <div className="box-s">
+            <h1 style={{ width: "20vw" }}>
+              <Skeleton />
+            </h1>
+
+            <form
+              className="form-s"
+              style={{ justifyContent: "space-between", marginTop: "20px" }}
+            >
+              <div className="inp-s">
+                <h1>
+                  <Skeleton height={50} />
+                </h1>
+              </div>
+              <div className="inp-s">
+                <h1>
+                  <Skeleton height={50} />
+                </h1>
+              </div>
+            </form>
+            <div className="data-s" style={{ marginTop: "25px" }}>
+              <h1 style={{ width: "20vw" }}>
+                <Skeleton />
+              </h1>
+              <h2 style={{ width: "20vw" }}>
+                <Skeleton />
+              </h2>
+              <h3 style={{ width: "20vw", marginBottom: "10px" }}>
+                <Skeleton />
+              </h3>
+              <Skeleton circle={true} height={70} width={70} />
+            </div>
+          </div>
+        </SkeletonTheme>
+      )}
+      ;
     </div>
   );
 }
